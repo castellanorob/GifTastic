e32c1b01265acad7a9dead429aa550185109e78b
Robert Castellano <castellanorob@gmail.com>
2018-01-07 21:26:07 -0600

    Updated html, css, js

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,63 +1,104 @@
-// Event listener for button elements
-
-$("button").on("click", function(){
-
-//array nintendo character names as strings
+// Array of character names 
 
 var characters = ["mario", "luigi", "princess peach", "bowser", "wario", "koopa"];
 
-
-//for loop that cycles through the array and creates buttons for each string
-    //(use something that appends for each string)
-
-for (var i = 0; i < characters.length; i++) {
-    document.createElement("button");
-}
-//console.log(characters);
-
-//link to Giphy API, displays 10 results
-
-var queryUrl = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=hZqu2JbYxTSrSs8rKHrSPZV6ZVhYYp30&limit=10";
-
-    //AJAX Get request
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    })
-
-    .done(function(response){
-        var results = response.data;
-    // Looping over every result item
-        for (var i = 0; i < results.length; i++) {
-        
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                // Creating a div with the class "item"
-                var gifDiv = $("<div class='item'>");
-        
-                // Storing the result item's rating
-                var rating = results[i].rating;
-        
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + rating);
+// displayCharacters function re-renders the HTML to display the appropriate content
+function displayCharacters() {
+    
+            var character = $(this).attr("data-name");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            character + "&api_key=hZqu2JbYxTSrSs8rKHrSPZV6ZVhYYp30&limit=10";
+    
             
-                // Creating an image tag
-                var personImage = $("<img>");
-            
-                // Giving the image tag an src attribute of a property pulled off the
-                // result item
-                personImage.attr("src", results[i].images.fixed_height.url);
-            
-                // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                gifDiv.append(p);
-                gifDiv.append(personImage);
-            
-                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                ("#gifs-appear-here").prepend(gifDiv);
+
+            // Creating an AJAX call for the specific character name button being clicked
+            $.ajax({
+              url: queryURL,
+              method: "GET"
+            }).done(function(response) {
+
+                //console.log(queryURL);
+                
+                //console.log(response);
+              // storing the data from the AJAX request in the results variable
+              var results = response.data;
+
+              for (var i = 0; i < results.length; i++) {
+              // Creating a div to hold the character gif
+              var characterDiv = $("<div class='character'>");
+
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + results[i].rating);
+    
+              // Creating an element to hold the image
+              var characterImage = $("<img>")
+              
+              // Setting the src attribute of the image to a property pulled off the result item
+              characterImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the image
+              characterDiv.append(p);
+              characterDiv.append(characterImage);
+    
+             
+              $("#gif-display").prepend(characterDiv);
+              }
+            });
+    
+          }
+         
+        // Function for displaying character data
+        function renderButtons() {
+    
+            // Deleting the characters prior to adding new characters
+            $("#name-display").empty();
+    
+            // Looping through the array of characters
+            for (var i = 0; i < characters.length; i++) {
+    
+              // Then dynamicaly generating buttons for each character in the array
+              var a = $("<button>");
+              // Adding a class of character to our button
+              a.addClass("character");
+              // Adding a data-attribute
+              a.attr("data-name", characters[i]);
+              // Providing the initial button text
+              a.text(characters[i]);
+              // Adding the button to the name-disply div
+              $("#name-display").append(a);
             }
         }
-    });
+    
+        // This function handles events where a character button is clicked
+        $("#add-character").on("click", function(event) {
+            event.preventDefault();
+            // This line grabs the input from the textbox
+            var character = $("#character-input").val().trim();
+    
+            // Adding character from the textbox to our array
+            characters.push(character);
+    
+            // Calling renderButtons which handles the processing of our character array
+            renderButtons();
+        });
 
-});
-//function that animates a gif when clicked
+        $("#gif-display").on("click", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+    
+          // Adding a click event listener to all elements with a class of "character"
+          $(document).on("click", ".character", displayCharacters);
+    
+          // Calling the renderButtons function to display the intial buttons
+          renderButtons();
